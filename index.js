f2dd61dd62ec5c8af3caffcb6605bb1d2760f5b7
lucasnix <lucasniix@gmail.com>
2023-04-24 06:14:19 -0300

    Adicionando trabalho de javascript mirim ao repositorio

---
+++ index.js
@@ -0,0 +1,11 @@
+//bracket Notation
+function greet(name, lastName) {
+   console.log('Ola ' + name + ' ' + lastName);
+}
+
+//calculo
+function blau(number) {
+    return number * number;
+}
+
+console.log(blau(900));
